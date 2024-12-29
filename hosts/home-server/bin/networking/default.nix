5365a60b1892ced7139829d2cd43745343548178
bou1der <krowkin@bk.ru>
2024-12-29 23:08:50 +0300

    Update env colmena

--- hosts/home-server/bin/networking/default.nix
+++ hosts/home-server/bin/networking/default.nix
@@ -9,7 +9,7 @@
     nat.internalInterfaces = [ "wg0" ];
 
     firewall.enable = true;
-    firewall.allowedTCPPorts = [ 22001 30033 25565 8000 6001 6002 80 443 3000 ];
+    firewall.allowedTCPPorts = [ 22001 30033 25565 8000 6001 6002 80 443 3000 18080 18081 18082 18083 18085 18089 ];
     firewall.allowedUDPPorts = [ 9987 51820 ];
   };
 }
